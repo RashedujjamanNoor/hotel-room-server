8e7d364cf8ad30857902b55195fb0b8b0955a8a3
RashedujjamanNoor <noorrk042@gmail.com>
2025-07-16 14:54:12 +0600

    create roomRoutes uploadMiddleware roomController

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ import { clerkMiddleware } from "@clerk/express";
 import clerkWebHooks from "./controllers/clerkWebhooks.js";
 import userRouter from "./routes/userRoutes.js";
 import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routes/roomRoutes.js";
 
 connectDB();
+connectCloudinary();
 
 const app = express();
 
@@ -23,6 +26,7 @@ app.use("/api/clerk", clerkWebHooks);
 app.get("/", (req, res) => res.send("API is working fine"));
 app.use("/api/user", userRouter);
 app.use("/api/hotels", hotelRouter);
+app.use("/api/rooms", roomRouter);
 
 const PORT = process.env.PORT || 3000;
 
